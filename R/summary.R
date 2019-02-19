eb42592dee7c8bc969bed4ed0bff11f0bfe22790
rathik <rathik@email.chop.edu>
2019-02-19 11:15:38 -0500

    update fusion summaries

--- R/summary.R
+++ R/summary.R
@@ -1,7 +1,8 @@
 ################################################
 # Author: Komal S Rathi
-# Function: Summarize fusion results for SV figure
-# Date: 12/18/2018
+# Function: Summarize fusion results
+# Date: 02/18/2019
+# Step 3
 ################################################
 
 library(ggplot2)
@@ -9,6 +10,7 @@ library(reshape2)
 library(dplyr)
 library(RColorBrewer)
 library(tidyr)
+library(xlsx)
 
 setwd('~/Projects/Maris-lab/PPTC_fusion_analysis/')
 source('~/Projects/Maris-lab/PPTC/R/pubTheme.R')
@@ -59,50 +61,8 @@ defuse.total <- unique(defuse[,c('Fused_Genes','Sample','Caller','Fusion_Type')]
 all.callers <- rbind(fc.total, sf.total, soapfuse.total, defuse.total)
 all.callers$Sample <- gsub('-R-human$|-R$|','',all.callers$Sample)
 
-# this is temporary
-# all.callers$GeneA <- gsub('--.*','',all.callers$Fused_Genes)
-# all.callers$GeneB <- gsub('.*--','',all.callers$Fused_Genes)
-
-# fix gene names from old to new
-# dat <- read.delim('data/2019-01-03-Hugo-Symbols-edited.txt', stringsAsFactors = F)
-# dat <- unique(dat[,c('Approved.symbol','Previous.symbols')])
-# dat <- dat[which(dat$Previous.symbols != ""),]
-# dat <- dat %>%
-#   mutate(Previous.symbols = strsplit(Previous.symbols, ",")) %>%
-#   unnest(Previous.symbols)
-# dat <- dat %>% 
-#   mutate(Previous.symbols = trimws(Previous.symbols, which= "both"),
-#          Approved.symbol = trimws(Approved.symbol, which = "both")) %>%
-#   mutate(Previous.symbols = gsub('@','',Previous.symbols),
-#          Approved.symbol = gsub('@','',Approved.symbol))
-# dat <- dat[which(dat$Approved.symbol != dat$Previous.symbols),]
-# 
-# tmp <- plyr::count(dat$Previous.symbols)
-# tmp <- tmp[which(tmp$freq == 1),]
-# dat <- dat[which(dat$Previous.symbols %in% tmp$x),]
-# rm(tmp)
-# all.callers$A.map <- ifelse(all.callers$GeneA %in% dat$Previous.symbols, 'Yes', 'No')
-# all.callers$B.map <- ifelse(all.callers$GeneB %in% dat$Previous.symbols, 'Yes', 'No')
-
-# for(i in 1:nrow(all.callers)){
-#   print(i)
-#   if(all.callers[i,'A.map'] == "Yes"){
-#     new.gene <- dat[dat$Previous.symbols %in% all.callers[i,'GeneA'],'Approved.symbol'] 
-#     all.callers[i,'GeneA'] <- new.gene
-#   }
-#   if(all.callers[i,'B.map'] == "Yes"){
-#     new.gene <- dat[dat$Previous.symbols %in% all.callers[i,'GeneB'],'Approved.symbol'] 
-#     all.callers[i,'GeneB'] <- new.gene
-#   }
-# }
-# all.callers$Fused_Genes <- paste0(all.callers$GeneA,'--',all.callers$GeneB)
-# all.callers <- unique(all.callers[,1:4])
-
-# remove @ genes
-# all.callers <- all.callers[grep('@', all.callers$Fused_Genes, invert = T),]
-
 # remove the samples that we don't need
-clin <- read.delim('data/2018-12-28-pdx-clinical-final-for-paper.txt', stringsAsFactors = F)
+clin <- read.delim('data/2019-02-09-pdx-clinical-final-for-paper.txt', stringsAsFactors = F)
 clin <- clin[which(clin$RNA.Part.of.PPTC == "yes"),]
 clin <- clin[,c('RNA.human.bam.filename','Model','Histology.Broad')]
 clin$RNA.human.bam.filename <- gsub('-R-human.bam$|-R_star_hg19_final.bam$|_star_hg19_final.bam$|-human.bam$','', clin$RNA.human.bam.filename)
@@ -113,7 +73,7 @@ hist.dt.ct <- plyr::count(hist.dt.ct, 'Histology.Broad')
 hist.dt.ct$freq <- paste0(hist.dt.ct$Histology.Broad,' (n=', hist.dt.ct$freq, ')')
 colnames(hist.dt.ct)[2] <- 'Histology.Broad.Label'
 
-# no fusions reported for NCH-RBD-1 so n = 247 here
+# n = 244
 to.remove <- setdiff(all.callers$Sample, clin$RNA.human.bam.filename)
 if(length(to.remove) > 0){
   all.callers <- all.callers[-which(all.callers$Sample %in% to.remove),]
@@ -137,7 +97,7 @@ all.callers.summary <- all.callers %>%
   unique() %>%
   as.data.frame()
 
-# or found in at least 2 samples of the same histology (n = 545)
+# or found in at least 2 samples of the same histology (n = 539)
 sample.count <- all.callers %>% 
   filter(Fusion_Type != "Other") %>%
   group_by(Fused_Genes, Histology.Broad) %>% 
@@ -167,11 +127,11 @@ rec2 <- unique(rec2[,c("Model","Fused_Genes","Histology.Broad")])
 rec3 <- unique(rec3[,c("Model","Fused_Genes","Histology.Broad")])
 res <- unique(rbind(rec2, rec3))
 
-# merge these (n = 6287)
+# merge these (n = 6169)
 total <- unique(rbind(all.callers.summary, sample.count, res))
 nrow(total)
 
-# remove fusions that are in > 1 histology (n = 1183)
+# remove fusions that are in > 1 histology (n = 1159)
 hist.count <- total %>% 
   select(Fused_Genes, Histology.Broad) %>%
   unique() %>%
@@ -181,7 +141,7 @@ hist.count <- total %>%
 total <- total[which(total$Fused_Genes %in% hist.count$Fused_Genes),]
 length(unique(total$Fused_Genes))
 
-# remove read-throughs or neighbouring genes (n = 944)
+# remove read-throughs or neighbouring genes (n = 922)
 sf.rt <- unique(sf[grep('readthrough|neighbors|GTEx_Recurrent',sf$annots),'Fused_Genes'])
 fc.rt <- unique(fc[grep('readthrough|adjacent|healthy',fc$Fusion_description),'Fused_Genes'])
 soapfuse.rt <- unique(soapfuse[grep('INTRACHR-SS-OGO-0GAP', soapfuse$Fusion_Cat),'Fused_Genes'])
@@ -195,19 +155,18 @@ length(unique(total$Fused_Genes))
 # add back MLL fusions by Chelsea, 
 # Pubmed fusions by Maria and 
 # literature fusions by Jo Lynne (generated using driver_fusions.R)
-drivers <- read.delim('~/Projects/Maris-lab/PPTC/data/pedcbio/pptc/data_fusions.txt', stringsAsFactors = F)
-drivers <- unique(drivers[,c('Fusion','Tumor_Sample_Barcode')])
-drivers <- merge(drivers, clin, by.x = 'Tumor_Sample_Barcode', by.y = 'Model')
+drivers <- read.delim('results/DriverFusions.txt', stringsAsFactors = F)
+drivers <- drivers[,c("Fused_Genes","Model")]
+drivers <- merge(drivers, clin, by = 'Model')
 drivers$RNA.human.bam.filename <- NULL
-drivers$Fusion <- gsub('-','--',drivers$Fusion)
-drivers$Fusion <- gsub('RP11--', 'RP11-', drivers$Fusion)
 colnames(drivers) <- colnames(total)
 # check for read-throughs (none should be present)
 length(intersect(drivers$Fused_Genes, rts))
 # check how many we missed or captured
-length(unique(setdiff(drivers$Fused_Genes, total$Fused_Genes))) # 37/94 unique fusions were missed
+length(unique(drivers$Fused_Genes)) # 92
+length(unique(setdiff(drivers$Fused_Genes, total$Fused_Genes))) # 38/92 unique fusions were missed
 total <- unique(rbind(total, drivers))
-length(unique(total$Fused_Genes)) # (n = 981)
+length(unique(total$Fused_Genes)) # (n = 960)
 
 # split in gene a and gene b
 total <- cbind(total, colsplit(total$Fused_Genes, '--', names = c('geneA', 'geneB')))
@@ -221,7 +180,68 @@ if(length(intersect(total$Fused_Genes, normals)) > 0){
 } else {
   total <- total
 }
-length(unique(total$Fused_Genes)) # 981
+length(unique(total$Fused_Genes)) # 960
+
+####### separate low expressing fusions and fusions where gene expression not reported
+load('data/pptc_rnaseq_hg38_matrix_v2.RData')
+rna.mat$not_expressed <- apply(rna.mat[,2:ncol(rna.mat)], 1, FUN = function(x) all(x < 1))
+df <- total
+df <- cbind(colsplit(df$Fused_Genes, pattern = '--', names = c("Gene1","Gene2")), df)
+genes <- unique(c(df$Gene1, df$Gene2))
+to.check <- setdiff(genes, rna.mat$gene_short_name) # 90
+rna.mat <- rna.mat[which(rna.mat$gene_short_name %in% genes),]
+rna.mat <- melt(rna.mat)
+rna.mat <- merge(rna.mat, clin[,c("Model","Histology.Broad")], by.x = 'variable', by.y = "Model")
+rna.mat$Histology.Broad <- as.character(rna.mat$Histology.Broad)
+
+# now add filter
+df$Gene1_model <- NA
+df$Gene1_hist_mean <- NA
+df$Gene1_mean <- NA
+df$Gene1_expr <- NA
+df$Gene2_model <- NA
+df$Gene2_hist_mean <- NA
+df$Gene2_mean <- NA
+df$Gene2_expr <- NA
+df$Gene1_not_expressed <- NA
+df$Gene2_not_expressed <- NA
+for(i in 1:nrow(df)){
+  print(i)
+  genea <- df[i,'Gene1']
+  geneb <- df[i,'Gene2']
+  model <- df[i,'Model']
+  hist <- df[i,'Histology.Broad']
+  hist <- gsub(' [(].*','', hist)
+  genea.expr <- unique(rna.mat[which(rna.mat$gene_short_name == genea),'not_expressed'])
+  geneb.expr <- unique(rna.mat[which(rna.mat$gene_short_name == geneb),'not_expressed'])
+  genea.val <- rna.mat[which(rna.mat$variable %in% model & rna.mat$gene_short_name == genea),'value']
+  geneb.val <- rna.mat[which(rna.mat$variable %in% model & rna.mat$gene_short_name == geneb),'value']
+  genea.hist.mean <- mean(rna.mat[which(rna.mat$gene_short_name == genea & rna.mat$Histology.Broad == hist),'value'])
+  geneb.hist.mean <- mean(rna.mat[which(rna.mat$gene_short_name == geneb & rna.mat$Histology.Broad == hist),'value'])
+  genea.mean <- mean(rna.mat[which(rna.mat$gene_short_name == genea),'value'])
+  geneb.mean <- mean(rna.mat[which(rna.mat$gene_short_name == geneb),'value'])
+  df[i,'Gene1_not_expressed'] <- ifelse(length(genea.expr) == 0, NA, genea.expr)
+  df[i,'Gene1_model'] <- ifelse(is.na(genea.val) || length(genea.val) == 0, NA, genea.val)
+  df[i,'Gene1_hist_mean'] <- ifelse(is.na(genea.hist.mean) || length(genea.hist.mean) == 0, NA, genea.hist.mean)
+  df[i,'Gene1_mean'] <- ifelse(is.na(genea.mean) || length(genea.mean) == 0, NA, genea.mean)
+  df[i,'Gene1_expr'] <- ifelse(df[i,'Gene1_model'] < df[i,'Gene1_mean'],'Decreased',ifelse(df[i,'Gene1_model'] == df[i,'Gene1_mean'], 'Same','Increased'))
+  df[i,'Gene2_not_expressed'] <- ifelse(length(geneb.expr) == 0, NA, geneb.expr)
+  df[i,'Gene2_model'] <- ifelse(is.na(geneb.val) || length(geneb.val) == 0, NA, geneb.val)
+  df[i,'Gene2_hist_mean'] <- ifelse(is.na(geneb.hist.mean) || length(geneb.hist.mean) == 0, NA, geneb.hist.mean)
+  df[i,'Gene2_mean'] <- ifelse(is.na(geneb.mean) || length(geneb.mean) == 0, NA, geneb.mean)
+  df[i,'Gene2_expr'] <- ifelse(df[i,'Gene2_model'] < df[i,'Gene2_mean'],'Decreased',ifelse(df[i,'Gene2_model'] == df[i,'Gene2_mean'], 'Same','Increased'))
+}
+df[is.na(df)] <- NA
+df$Gene1_not_expressed[is.na(df$Gene1_not_expressed)] <- "Not Reported"
+df$Gene2_not_expressed[is.na(df$Gene2_not_expressed)] <- "Not Reported"
+separate.fusions <- df[(df$Gene1_not_expressed == "Not Reported" & df$Gene2_not_expressed == "Not Reported") | (df$Gene1_not_expressed == TRUE & df$Gene2_not_expressed == TRUE),]
+if(nrow(separate.fusions) > 0){
+  print("Fusions to be separated")
+  write.table(separate.fusions, file = 'results/Filtered_Annotated_Fusions_noExprReported.txt', quote = F, sep = "\t", row.names = F)
+  df <- df[-which(df$Fused_Genes %in% separate.fusions$Fused_Genes),]
+}
+total <- total[which(total$Fused_Genes %in% df$Fused_Genes),]
+####### separate low expressing fusions and fusions where gene expression not reported 
 
 # add Cytogenetics info
 cyto <- read.delim('results/DriverFusions.txt', stringsAsFactors = F)
@@ -253,11 +273,12 @@ for.table$Kinase <- ifelse(for.table$geneA %in% kinase$gene_symbol | for.table$g
 tf <- read.delim('~/Projects/Maris-lab/NBL-Fusion-Analysis/data/TRANSFAC_TF.txt', header = F)
 for.table$TF <- ifelse(for.table$geneA %in% tf$V1 | for.table$geneB %in% tf$V1, 'Yes', 'No')
 
-# n = 306
+# n = 284
 for.table <- unique(for.table[which(for.table$Cosmic == "Yes" | for.table$Kinase == "Yes" | for.table$TF == "Yes"),])
-setdiff(drivers$Fused_Genes, for.table$Fused_Genes) # 5 fusions are missing
+length(setdiff(drivers$Fused_Genes, for.table$Fused_Genes)) # 7 fusions are missing
 for.table$geneA <- NULL
 for.table$geneB <- NULL
+nrow(for.table)
 
 # excel tab 2
 extab2 <- merge(for.table, hist.dt.ct, by = 'Histology.Broad')
@@ -269,9 +290,16 @@ extab2 <- extab2 %>%
 extab2 <- unique(extab2[,c("Fused_Genes","Cosmic","TF","Kinase")])
 extab1 <- merge(extab1, extab2, by = 'Fused_Genes', all.x = T)
 extab1[is.na(extab1)] <- "No"
-# 2884
+# 2813
 colnames(extab1)[4] <- 'Method'
+nrow(extab1)
 write.table(extab1, file = 'results/Filtered_Annotated_Fusions.txt', quote = F, sep = "\t", row.names = F)
+
+# write to excel
+tab1 <- read.delim('results/DriverFusions.txt', stringsAsFactors = F)
+tab2 <- read.delim('results/Filtered_Annotated_Fusions.txt', stringsAsFactors = F)
+write.xlsx(x = tab1, file = 'results/FusionTable.xlsx', sheetName = "Driver_Fusions", row.names = F)
+write.xlsx(x = tab2, file = 'results/FusionTable.xlsx', sheetName = "Filtered_Annotated_Fusions", row.names = F, append = TRUE)
 
 #########
 # plot for fusions only
@@ -279,7 +307,7 @@ write.table(extab1, file = 'results/Filtered_Annotated_Fusions.txt', quote = F, 
 total$geneA <- NULL
 total$geneB <- NULL
 
-clin <- read.delim('data/2018-12-28-pdx-clinical-final-for-paper.txt', stringsAsFactors = F)
+clin <- read.delim('data/2019-02-09-pdx-clinical-final-for-paper.txt', stringsAsFactors = F)
 clin <- clin[which(clin$RNA.Part.of.PPTC == "yes"),]
 clin <- clin[,c('RNA.human.bam.filename','Model','Histology.Detailed')]
 clin$RNA.human.bam.filename <- gsub('-R-human.bam$|-R_star_hg19_final.bam$|_star_hg19_final.bam$','', clin$RNA.human.bam.filename)
@@ -302,7 +330,7 @@ final <- rbind(final, data.frame(Model = c(rep(NA,length(to.include))),
 final$Histology.Detailed <- reorder(final$Histology.Detailed, final$median)
 write.table(final, file = 'results/FusionPlot_rawdata.txt', quote = F, sep = "\t", row.names = F)
 
-cols <- read.delim('~/Projects/Maris-lab/PPTC/data/figures/2018-08-23-all-hist-colors', header = F, stringsAsFactors = F)
+cols <- read.delim('~/Projects/Maris-lab/PPTC/data/figures/2019-02-09-all-hist-colors.txt', header = F, stringsAsFactors = F)
 cols <- cols[which(cols$V1 %in% final$Histology.Detailed),]
 cols <- cols[match(levels(final$Histology.Detailed), cols$V1),] # reorder colors to match histology
 group.colors <- setNames(cols$V2, nm = cols$V1)
@@ -318,6 +346,7 @@ p <- ggplot(final, aes(x = Histology.Detailed, y = value, color = Histology.Deta
   guides(color = F) +
   scale_color_manual(values = group.colors) +
   scale_y_continuous(breaks = seq(0, 26, by = 5))
+p
 ggsave(filename = 'results/FusionPlot.pdf', plot = p, device = 'pdf', height = 6, width = 10)
 
 medians <- unique(final[,c("Histology.Detailed","median")])
