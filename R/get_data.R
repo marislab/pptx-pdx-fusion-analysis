71cef6bfc9767bcefd489da3c4954d3f104e327d
rathik <rathik@email.chop.edu>
2019-03-12 12:48:43 -0400

    pulls data from figshare

--- R/get_data.R
+++ R/get_data.R
@@ -7,22 +7,39 @@
 
 # RNA-seq expression matrix in hg38
 system("wget --output-document='data/pptc_rnaseq_hg38_matrix.RData' \
-	https://ndownloader.figshare.com/files/14452982")
+       https://ndownloader.figshare.com/files/14452982")
 
 # RNA-seq expression matrix in hg19 (for pedcbio)
 system("wget --output-document='data/pedcbio/2019-02-14-PPTC_FPKM_matrix_withModelID-244.rda' \
-	https://ndownloader.figshare.com/files/14452985")
+       https://ndownloader.figshare.com/files/14452985")
 
 # fusion input
 # defuse
 system("wget --output-document='data/Defuse_PPTC_RNASeq_hg19.txt' \
-	https://ndownloader.figshare.com/files/14460299")
+       https://ndownloader.figshare.com/files/14460299")
 # soapfuse
 system("wget --output-document='data/SOAPfuse_PPTC_RNASeq_hg19.txt' \
-	https://ndownloader.figshare.com/files/14460305")
+       https://ndownloader.figshare.com/files/14460305")
 # starfusion
 system("wget --output-document='data/starfusion_PPTC_RNASeq_hg19.txt' \
-	https://ndownloader.figshare.com/files/14460308")
+       https://ndownloader.figshare.com/files/14460308")
 # fusioncatcher
 system("wget --output-document='data/fusioncatcher_PPTC_RNASeq_hg19.txt' \
-	https://ndownloader.figshare.com/files/14460302")
+       https://ndownloader.figshare.com/files/14460302")
+# from BCM and Australia group
+system("wget --output-document='data/PPTC_Fusion_Human-MC.txt' \
+       https://ndownloader.figshare.com/files/14571029")
+system("wget --output-document='data/ALL_cytogenetics_translocations_CM.txt' \
+       https://ndownloader.figshare.com/files/14571032")
+
+# gene annotations
+system("wget --output-document='data/kinases.txt' \
+       https://ndownloader.figshare.com/files/14571038")
+system("wget --output-document='data/TRANSFAC_TF.txt' \
+       https://ndownloader.figshare.com/files/14571035")
+system("wget --output-document='data/Cosmic_gene_census.csv' \
+       https://ndownloader.figshare.com/files/14571041")
+
+# for pedcbio (gene mapping)
+system("wget --output-document='data/genes.tsv' \
+       https://ndownloader.figshare.com/files/14571026")
